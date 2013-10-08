b145810de9cf280cdc3f3db3e0ce2bb6ac044ca1
Jeffrey Mullins <jmullins@techresidents.com>
2013-10-08 15:10:40 -0400

    Changing trhttp_gevent and trrackpace dependencies to integration.

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ setup(
         ],
     install_requires=[
         'trpycore>=0.12.0',
-        'trhttp_gevent>=0.6.0',
-        'trrackspace>=0.4.0',
+        'trhttp_gevent>=0.7.0',
+        'trrackspace>=0.5.0',
     ],
     dependency_links=[
         'git+ssh://dev.techresidents.com/tr/repos/techresidents/lib/python/trpycore.git@0.12.0#egg=trpycore-0.12.0',
-        'git+ssh://dev.techresidents.com/tr/repos/techresidents/lib/python/trhttp_gevent.git@0.6.0#egg=trhttp_gevent-0.6.0',
-        'git+ssh://dev.techresidents.com/tr/repos/techresidents/lib/python/trrackspace.git@0.4.0#egg=trrackspace-0.4.0'
+        'git+ssh://dev.techresidents.com/tr/repos/techresidents/lib/python/trhttp_gevent.git@0.7.0#egg=trhttp_gevent-0.7.0',
+        'git+ssh://dev.techresidents.com/tr/repos/techresidents/lib/python/trrackspace.git@0.5.0#egg=trrackspace-0.5.0'
     ],
 )
